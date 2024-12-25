f83f52f884fcc6ee47754ff6aa6de9e35e6029d3
meddhawi <meddhawi@gmail.com>
2024-12-25 12:53:23 +0700

    changed codes

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -1,60 +1,71 @@
 ﻿Public Class Form1
-    Private firstNumber As Double
-    Private operation As String
-    Private newNumber As Boolean = True
+    ' Variabel untuk menyimpan angka pertama yang diinput
+    Private angkaPertama As Double
+    ' Variabel untuk menyimpan jenis operasi matematika
+    Private jenisOperasi As String
+    ' Variabel untuk mengecek apakah angka yang diinput adalah angka baru
+    Private angkaBaru As Boolean = True
 
-    Private Sub NumberButton_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
-        Dim button As Button = DirectCast(sender, Button)
+    ' Fungsi untuk menangani tombol angka
+    Private Sub TombolAngka_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+        Dim tombol As Button = DirectCast(sender, Button)
         
-        If newNumber Then
-            txtDisplay.Text = button.Text
-            newNumber = False
+        ' Jika angka baru, ganti text di layar
+        ' Jika bukan angka baru, tambahkan angka di belakangnya
+        If angkaBaru Then
+            txtDisplay.Text = tombol.Text
+            angkaBaru = False
         Else
-            txtDisplay.Text &= button.Text
+            txtDisplay.Text &= tombol.Text
         End If
     End Sub
 
-    Private Sub OperatorButton_Click(sender As Object, e As EventArgs) Handles btnAdd.Click, btnSubtract.Click, btnMultiply.Click, btnDivide.Click
-        Dim button As Button = DirectCast(sender, Button)
+    ' Fungsi untuk menangani tombol operator (+, -, x, ÷)
+    Private Sub TombolOperator_Click(sender As Object, e As EventArgs) Handles btnAdd.Click, btnSubtract.Click, btnMultiply.Click, btnDivide.Click
+        Dim tombol As Button = DirectCast(sender, Button)
         
-        If Not newNumber Then
-            firstNumber = Double.Parse(txtDisplay.Text)
-            operation = button.Text
-            newNumber = True
+        If Not angkaBaru Then
+            angkaPertama = Double.Parse(txtDisplay.Text)
+            jenisOperasi = tombol.Text
+            angkaBaru = True
         End If
     End Sub
 
-    Private Sub btnEquals_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
-        If Not newNumber Then
-            Dim secondNumber As Double = Double.Parse(txtDisplay.Text)
-            Dim result As Double
+    ' Fungsi untuk menangani tombol sama dengan (=)
+    Private Sub TombolSamaDengan_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
+        If Not angkaBaru Then
+            Dim angkaKedua As Double = Double.Parse(txtDisplay.Text)
+            Dim hasil As Double
 
-            Select Case operation
+            ' Pilih operasi yang akan dilakukan
+            Select Case jenisOperasi
                 Case "+"
-                    result = firstNumber + secondNumber
+                    hasil = angkaPertama + angkaKedua
                 Case "-"
-                    result = firstNumber - secondNumber
+                    hasil = angkaPertama - angkaKedua
                 Case "×"
-                    result = firstNumber * secondNumber
+                    hasil = angkaPertama * angkaKedua
                 Case "÷"
-                    If secondNumber <> 0 Then
-                        result = firstNumber / secondNumber
+                    ' Cek pembagian dengan nol
+                    If angkaKedua <> 0 Then
+                        hasil = angkaPertama / angkaKedua
                     Else
-                        MessageBox.Show("Cannot divide by zero!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                        btnClear_Click(Nothing, Nothing)
+                        MessageBox.Show("Tidak bisa membagi dengan nol!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        TombolHapus_Click(Nothing, Nothing)
                         Return
                     End If
             End Select
 
-            txtDisplay.Text = result.ToString()
-            newNumber = True
+            txtDisplay.Text = hasil.ToString()
+            angkaBaru = True
         End If
     End Sub
 
-    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+    ' Fungsi untuk menangani tombol hapus (Clear)
+    Private Sub TombolHapus_Click(sender As Object, e As EventArgs) Handles btnClear.Click
         txtDisplay.Text = "0"
-        firstNumber = 0
-        operation = ""
-        newNumber = True
+        angkaPertama = 0
+        jenisOperasi = ""
+        angkaBaru = True
     End Sub
 End Class
